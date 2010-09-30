4e975c55d6dd7e8c75700f46d775c74ad2d7fa24
Peter Brown <pete@lette.us>
2010-09-30 16:09:21 -0400

    changed how ClassyEnumAttributes extends active record

--- lib/classy_enum.rb
+++ lib/classy_enum.rb
@@ -69,37 +69,32 @@ module ClassyEnum
 end
 
 module ClassyEnumAttributes
-  module ClassMethods
-    def classy_enum_attr(klass, method=nil)
-      
-      method ||= klass
-      
-      klass = klass.to_s.camelize.constantize
-      
-      # Add ActiveRecord validation to ensure it won't be saved unless it's an option
-      self.send(:validates_inclusion_of, method, :in => klass.all)
-      
-      self.instance_eval do
-        
-        # Define getter method
-        define_method method do
-          klass.new(super)
-        end
-        
-        # Define setter method
-        define_method "#{method}=" do |value|
-          super value.to_s
-        end
-        
+
+  def classy_enum_attr(klass, method=nil)
+
+    method ||= klass
+
+    klass = klass.to_s.camelize.constantize
+
+    # Add ActiveRecord validation to ensure it won't be saved unless it's an option
+    self.send(:validates_inclusion_of, method, :in => klass.all)
+
+    self.instance_eval do
+
+      # Define getter method
+      define_method method do
+        klass.new(super)
       end
-      
+
+      # Define setter method
+      define_method "#{method}=" do |value|
+        super value.to_s
+      end
+
     end
+
   end
-  
-  def self.included(other)
-    other.extend ClassMethods
-  end
-  
+
 end
 
-ActiveRecord::Base.send :include, ClassyEnumAttributes
+ActiveRecord::Base.send :extend, ClassyEnumAttributes
