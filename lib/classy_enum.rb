5f4711f75c0e85c6a72c3844d96af5729380a91c
Peter Brown <pete@lette.us>
2010-09-30 16:58:35 -0400

    Moved classy_enum_attributes module to sepearate file

--- lib/classy_enum.rb
+++ lib/classy_enum.rb
@@ -1,3 +1,5 @@
+require "classy_enum/classy_enum_attributes"
+
 class ClassyEnumValue < Object 
  
   attr_reader :to_s, :index
@@ -68,33 +70,4 @@ module ClassyEnum
   
 end
 
-module ClassyEnumAttributes
 
-  def classy_enum_attr(klass, method=nil)
-
-    method ||= klass
-
-    klass = klass.to_s.camelize.constantize
-
-    # Add ActiveRecord validation to ensure it won't be saved unless it's an option
-    self.send(:validates_inclusion_of, method, :in => klass.all)
-
-    self.instance_eval do
-
-      # Define getter method
-      define_method method do
-        klass.new(super)
-      end
-
-      # Define setter method
-      define_method "#{method}=" do |value|
-        super value.to_s
-      end
-
-    end
-
-  end
-
-end
-
-ActiveRecord::Base.send :extend, ClassyEnumAttributes
