2f13f81f7f20d87407a47630c6836738ebd1acb0
Peter Brown <pete@lette.us>
2010-10-15 01:27:23 -0400

    Added Rails 3 enum generator

---
+++ lib/generators/enum/enum_generator.rb
@@ -0,0 +1,18 @@
+class EnumGenerator < Rails::Generators::NamedBase
+  desc "Generate a ClassyEnum definition in app/enums/"
+
+  argument :name, :type => :string, :required => true, :banner => 'EnumName'
+  argument :values, :type => :array, :default => [], :banner => 'value1 value2 value3 etc...'
+
+  source_root File.expand_path("../templates", __FILE__)
+
+  #def self.banner
+  #  "rails generate enum NewEnum value1 value2 value3"
+  #end
+  
+  def copy_files
+    empty_directory 'app/enums'
+    template "enum.erb", "app/enums/#{file_name}.rb"
+  end
+
+end
