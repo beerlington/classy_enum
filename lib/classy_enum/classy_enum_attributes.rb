396da68f1202d533fc8b11f4f989857c91e19972
beerlington <pete@lette.us>
2010-11-02 00:44:22 -0400

    Changed namespace of classy enum attributes

--- lib/classy_enum/classy_enum_attributes.rb
+++ lib/classy_enum/classy_enum_attributes.rb
@@ -1,30 +1,32 @@
-module ClassyEnumAttributes
+module ClassyEnum
+  module Attributes
 
-  def classy_enum_attr(klass, method=nil)
+    def classy_enum_attr(klass, method=nil)
 
-    method ||= klass
+      method ||= klass
 
-    klass = klass.to_s.camelize.constantize
+      klass = klass.to_s.camelize.constantize
 
-    # Add ActiveRecord validation to ensure it won't be saved unless it's an option
-    self.send(:validates_inclusion_of, method, :in => klass.all, :allow_nil => true)
+      # Add ActiveRecord validation to ensure it won't be saved unless it's an option
+      self.send(:validates_inclusion_of, method, :in => klass.all, :allow_nil => true)
 
-    self.instance_eval do
+      self.instance_eval do
 
-      # Define getter method
-      define_method method do
-        klass.new(super())
-      end
+        # Define getter method
+        define_method method do
+          klass.new(super())
+        end
 
-      # Define setter method
-      define_method "#{method}=" do |value|
-        super(value.to_s)
+        # Define setter method
+        define_method "#{method}=" do |value|
+          super(value.to_s)
+        end
+
       end
 
     end
 
   end
-
 end
 
-ActiveRecord::Base.send :extend, ClassyEnumAttributes
+ActiveRecord::Base.send :extend, ClassyEnum::Attributes
