da9f8fdd50a7c3d898825c4c531398e5f31f3c47
beerlington <pete@lette.us>
2011-01-29 10:19:19 -0500

    Updated generators to make enum inheritance more explicit

--- generators/templates/enum.rb
+++ generators/templates/enum.rb
@@ -2,6 +2,6 @@ class <%= class_name %> < ClassyEnum::Base
   enum_classes <%= args.map {|a| ":#{a}"}.join(", ") %>
 end
 <% args.each do |arg| %>
-class <%= class_name + arg.camelize %>
+class <%= class_name + arg.camelize %> < <%= class_name %>
 end
 <% end %>
