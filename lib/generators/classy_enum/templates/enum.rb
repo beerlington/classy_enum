4ce1b787ad6388b537eddc47220ed87472e6cc56
beerlington <pete@lette.us>
2012-07-15 11:25:37 -0400

    Update generator to produce classes with new namespacing

--- lib/generators/classy_enum/templates/enum.rb
+++ lib/generators/classy_enum/templates/enum.rb
@@ -2,6 +2,6 @@ class <%= class_name %> < ClassyEnum::Base
   enum_classes <%= values.map {|a| ":#{a}"}.join(", ") %>
 end
 <% values.each do |arg| %>
-class <%= class_name + arg.camelize %> < <%= class_name %>
+class <%= "#{class_name}::#{arg.camelize}" %> < <%= class_name %>
 end
-<% end %>
+<%- end -%>
