e0a59054ec01f1214d25e89e434660137729a27e
Kristian Mandrup <kmandrup@gmail.com>
2012-09-17 11:45:24 +0200

    fixed engine

--- lib/classy_enum/railtie.rb
+++ lib/classy_enum/railtie.rb
@@ -1,6 +1,6 @@
 module ClassyEnum
   module Rails
-    module Railtie < ::Rails::Railtie
+    class Railtie < ::Rails::Railtie
       initializer "classy enum" do |app|
         ActiveSupport::Dependencies.autoload_paths << "#{Rails.root}/app/enums"
       end
