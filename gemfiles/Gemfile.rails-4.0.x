6c14930785cb546b0165b3f8805681e8575b8840
beerlington <pete@lette.us>
2013-05-12 11:18:53 -0400

    Adding Rails 4 travis support

---
+++ gemfiles/Gemfile.rails-4.0.x
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem 'rails', '4.0.0.rc1'
+gem 'rspec-rails', '~> 2.11'
+gem 'sqlite3'
+gem 'json', '~> 1.7'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
+require 'rails'
 require 'active_record'
 require 'action_view'
 require 'action_controller'
